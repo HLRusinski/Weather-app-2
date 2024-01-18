b67101b86b88040d8979be299af56059789a04c6
HLRusinski <156833957+HLRusinski@users.noreply.github.com>
2024-01-18 22:06:10 +0000

    added id to div element and added function to take the search input and pull through live temperature using api

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
+function updateWeather(response) {
+  let tempElement = document.querySelector("#current-temp");
+  let temperature = response.data.temperature.current;
+  tempElement.innerHTML = Math.round(temperature);
+}
+
+function pullTemp(city) {
+  let apiKey = "b0ct14bcbb3obb233faba3e22b01a08b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  pullTemp(searchInput.value);
 }
 
 let citySearchElement = document.querySelector("#city-search");
